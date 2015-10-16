c75909556cc74ed78905d6ffdefb949906ef0f55
Christos Kapodistrias <kapodistrias@Christoss-MacBook-Pro.local>
2015-10-16 16:54:14 +0200

    gemstorefinal

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+
+(function(){
 var app = angular.module('gemStore', [ ]);
+
+app.controller('StoreContorller', function(){
+	this.product = gem;
+});
 
 var gem = {
 	name: '',
 	price: 2.95,
 	description: '. . .'
 }
+
+})();
